19104b440e14c863057645ee5774b69cb13d5db4
John Henry Thompson <jht9629@nyu.edu>
2024-03-28 12:04:12 -0400

    mirror

--- downloads/gen/download.sh
+++
@@ -1,4 +0,0 @@
-echo download 1 "ims04-jht scroll color rate-2pxhnehBV"
-curl -s https://editor.p5js.org/editor/projects/2pxhnehBV/zip -o "/Users/jht2/Documents/projects/2024/p5mo/p5mirror-jht9629-nyu/downloads/zips/ims04-jht scroll color rate-2pxhnehBV.zip"
-echo download 2 "p5moExamples lobby-vP6sWN4Cu"
-curl -s https://editor.p5js.org/editor/projects/vP6sWN4Cu/zip -o "/Users/jht2/Documents/projects/2024/p5mo/p5mirror-jht9629-nyu/downloads/zips/p5moExamples lobby-vP6sWN4Cu.zip"
--- downloads/gen/unzip.sh
+++ downloads/gen/unzip.sh
@@ -1,18 +1,4 @@
 cd "/Users/jht2/Documents/projects/2024/p5mo/p5mirror-jht9629-nyu/downloads/../p5projects"
-#
-echo unzip 1 "ims04-jht scroll color rate-2pxhnehBV"
-rm -rf "./ims04-jht scroll color rate-2pxhnehBV"
-mkdir "./ims04-jht scroll color rate-2pxhnehBV"
-pushd "./ims04-jht scroll color rate-2pxhnehBV" > /dev/null
-unzip -q "../../downloads/zips/ims04-jht scroll color rate-2pxhnehBV"
-popd > /dev/null
-#
-echo unzip 2 "p5moExamples lobby-vP6sWN4Cu"
-rm -rf "./p5moExamples lobby-vP6sWN4Cu"
-mkdir "./p5moExamples lobby-vP6sWN4Cu"
-pushd "./p5moExamples lobby-vP6sWN4Cu" > /dev/null
-unzip -q "../../downloads/zips/p5moExamples lobby-vP6sWN4Cu"
-popd > /dev/null
 
 cd ..
 # remove redundant p5.js p5.sound.min.js
