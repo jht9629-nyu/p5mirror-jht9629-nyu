7f8df65e25a4b16233f5f49cd8ed0a796e93c4d0
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-11-14 15:16:18 -0500

    rm -f p5projects/*/p5.* p5projects/*/ml5.min.js

--- downloads/gen/unzip.sh
+++ downloads/gen/unzip.sh
@@ -1,15 +1,8 @@
 cd "/Users/jht2/Documents/projects/_2025/p5-mirrors/p5mirror-jht9629-nyu/downloads/../p5projects"
-#
-echo unzip 1 "Conditionals - Bouncing Ball with Gravity v16-RM_xP4Epj"
-rm -rf "./Conditionals - Bouncing Ball with Gravity v16-RM_xP4Epj"
-mkdir "./Conditionals - Bouncing Ball with Gravity v16-RM_xP4Epj"
-pushd "./Conditionals - Bouncing Ball with Gravity v16-RM_xP4Epj" > /dev/null
-unzip -q "../../downloads/zips/Conditionals - Bouncing Ball with Gravity v16-RM_xP4Epj"
-popd > /dev/null
 
 cd ..
-# remove redundant p5.js p5.sound.min.js
-rm -f p5projects/*/p5.*
+# remove redundant p5.js p5.sound.min.js ml5.min.js
+rm -f p5projects/*/p5.* p5projects/*/ml5.min.js
 # sync last_updatedAt.txt
 cd downloads/json
 if [ -e pending_updatedAt.txt ]; then
